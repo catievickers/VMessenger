7e398de2fbb3c040df2faabcb38525acce810ce6
Sam Osman <sam.osman97@gmail.com>
2018-12-02 16:46:22 -0700

    Layout for Adapter Design Pattern

---
+++ NativeLoginAdaptee.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class NativeLoginAdaptee extends React.Component {
+  static render() {
+    return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Log in with VMessenger</Text>
+				<TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 100}}
+		autoCorrect={false}
+		textContentType='username'
+		placeholder='Username'
+        onChange={(text1) => this.setState({text1})}
+        value={this.state.text1}
+      />
+	  
+	  <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 100}}
+		autoCorrect={false}
+		textContentType = 'password'
+		placeholder='Password'
+        onChange={(text2) => this.setState({text2})}
+        value={this.state.text2}
+      />
+	  
+                <Button title="Log in" onPress= "login()" />
+            </View>
+    );
+  }
+  
+  login(){
+	  
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
